800640c99124f14292dc5f55ce0b9625ede1fbb0
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-12-06 15:37:07 -0600

    updated

--- movies.js
+++ movies.js
@@ -28,8 +28,6 @@ function generateMovies() {
         });
     }
 
-
-
     document.getElementById('movieForm').addEventListener('submit', function (event) {
         event.preventDefault();
 
@@ -54,45 +52,91 @@ function generateMovies() {
                 })
                 .catch(error => console.error('Error fetching data:', error))
 
-
-        } else
-    {
-        // Handle the case when the input is empty
-        console.error('Please enter a movie title');
-    }
+        } else {
+            // Handle the case when the input is empty
+            console.error('Please enter a movie title');
+        }
     });
 }
 
 generateMovies();
 
-    function addMovie() {
-        // Retrieve values from the form
-        let title = document.getElementById('title').value;
-        let description = document.getElementById('description').value;
-        let rating = document.getElementById('rating').value;
+function addMovie() {
+    // Retrieve values from the form
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let year = document.getElementById('year').value;
 
-        // Create a new movie card element
-        let movieContainer = document.getElementById('movie-container');
-        let newMovieCard = document.createElement('div');
-        newMovieCard.classList.add('col-md-4');
-        newMovieCard.innerHTML = `
+    // Create a new movie card element
+    let movieContainer = document.getElementById('movie-container');
+    let newMovieCard = document.createElement('div');
+    newMovieCard.classList.add('col-md-4');
+    newMovieCard.innerHTML = `
         <div class="card mb-4">
             <div class="card-body">
                 <h5 class="card-title">${title}</h5>
                 <p class="card-text">${description}</p>
-                <p class="card-text">Rating: ${rating}</p>
+                <p class="card-text">Year: ${year}</p>
                 <button class="btn btn-danger" onclick="deleteMovie(this)">Delete</button>
             </div>
         </div>
     `;
 
-        // Append the new movie card to the movie container
-        movieContainer.appendChild(newMovieCard);
-    }
+    // Append the new movie card to the movie container
+    movieContainer.appendChild(newMovieCard);
 
-    function deleteMovie(button) {
-        // Traverse the DOM to remove the entire movie card
-        let movieCard = button.parentNode.parentNode.parentNode; // Assuming structure: button -> div.card-body -> div.card -> movie card itself
-        movieCard.remove();
+    // Example of usage
+    const newMovieData = {
+        title: title,
+        description: description,
+        year: year
+    };
+
+    createMovie(newMovieData)
+        .then(newMovie => {
+            console.log('New movie added:', newMovie);
+            // Update your UI or perform additional actions as needed
+        })
+        .catch(error => console.error('Error creating movie:', error));
+}
+
+// function deleteMovie(button) {
+//     // Traverse the DOM to remove the entire movie card
+//     let movieCard = button.parentNode.parentNode.parentNode; // Assuming structure: button -> div.card-body -> div.card -> movie card itself
+//     movieCard.remove();
+// }
+
+async function createMovie(movie) {
+    try {
+        const url = 'http://localhost:3000/movies';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(movie)
+        };
+        const response = await fetch(url, options);
+        const newMovie = await response.json();
+        // console.log(newMovie)
+        return newMovie;
+    } catch (error) {
+        console.error(error);
+        throw error; // Re-throw the error to handle it outside
     }
+}
+
+function deleteMovie(button) {
+    let movieCard = button.parentNode.parentNode.parentNode;
+    let movieTitle = movieCard.querySelector('.card-title').innerText;
+
+    // Example of usage
+    deleteMovieOnServer(movieTitle)
+        .then(deletedMovie => {
+            console.log('Movie deleted:', deletedMovie);
+            // Remove the movie card from the UI
+            movieCard.remove();
+        })
+        .catch(error => console.error('Error deleting movie:', error));
+}
 
