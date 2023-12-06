e129b4fc9d568dc31f88c4e334e0c1835b2b8f7d
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-12-06 11:26:41 -0600

    updated exercise

--- movies.js
+++ movies.js
@@ -1,37 +1,45 @@
-'use strict'
+'use strict';
 
 function generateMovies() {
-
-    const loader = document.getElementsByClassName("spinner")[0];
+    const loader = document.getElementsByClassName("popcorn")[0];
+    const movieListContainer = document.getElementById("movieList"); // Assuming you have a container with id="movieList"
 
     const delay = 5000; // delay time in milliseconds
 
-    loader.style.display = "block"
+    loader.style.display = "block";
 
     fetch('http://localhost:3000/movies')
-
         .then(response => response.json())
         .then(movies => {
-            console.log(movies)
+            // Assuming movies is an array of objects with properties like 'title' and 'description'
+            renderMovies(movies);
         })
-        .catch(error => console.error(error));
+        .catch(error => console.error(error))
+        .finally(() => {
+            // Ensure the loader is hidden even in case of an error
+            setTimeout(() => {
+                loader.style.display = "none";
+            }, delay);
+        });
 
-    setTimeout(() => {
-        loader.style.display = "none";
-    }, delay);
+    function renderMovies(movies) {
+        // Clear existing content
+        movieListContainer.innerHTML = '';
+
+        // Iterate through movies and create HTML elements
+        movies.forEach(movie => {
+            const movieElement = document.createElement('div');
+            movieElement.innerHTML = `
+                <h2>${movie.title}</h2>
+                <p>${movie.description}</p>
+                <!-- Add more details as needed -->
+
+                <hr>
+            `;
+            movieListContainer.appendChild(movieElement);
+        });
+    }
 }
-function createPoster() {
-    const movieTitle = ""; // Replace 'your_movie_title' with the actual movie title
-    const posterUrl = `http://img.omdbapi.com/?apikey=${MOVIE_API_KEY}&t=${movieTitle}`;
 
-    fetch(posterUrl)
-        .then(response => response.json())
-        .then(posterData => {
-            console.log(posterData);
-            // Handle the poster data as needed
-        })
-        .catch(error => console.log(error));
-}
+generateMovies();
 
-    generateMovies()
-    createPoster()
